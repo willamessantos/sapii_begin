6c2a7f1e39ea05de65abcf32c9efeca7b0ff9742
willamessantos <41154957+willamessantos@users.noreply.github.com>
2024-04-11 23:30:24 +0000

    adiciona scripts `lint:check` e `lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1> Priscila é o amor da minha vida, caso tenha visto essa mensagem mande um coraçãozin no meu zap  </h1>
+function Home() {
+  return (
+    <h1>
+      {" "}
+      Priscila é o amor da minha vida, caso tenha visto essa mensagem mande um
+      coraçãozin no meu zap{" "}
+    </h1>
+  );
 }
 
 export default Home;
